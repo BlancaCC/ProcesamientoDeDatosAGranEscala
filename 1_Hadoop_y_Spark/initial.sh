d09a8ca32aee388374cb2e8289e6679e78f6f0d4
Blanca <blancacano2.71828@gmail.com>
2022-09-29 12:26:33 +0200

    Script sobre la instalación de hadoop en modo standalone (no relevante para la práctica 1

--- 1_Hadoop_y_Spark/initial.sh
+++ 1_Hadoop_y_Spark/initial.sh
@@ -12,6 +12,8 @@ mv hadoop-2.8.1 opt/
 cd opt/ && ln -s hadoop-2.8.1 hadoop
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk
 echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk' >> ./hadoop/etc/hadoop/hadoop-env.sh
-./hadoop/bin/hadoop 
+# Comando de prueba
+#./opt/hadoop/bin/hadoop 
 
 #cat opt/hadoop/etc/hadoop/hadoop-env.sh
+yum -y install openssh-server openssh-clients
